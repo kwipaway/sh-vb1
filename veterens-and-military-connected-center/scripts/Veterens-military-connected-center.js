caaf495c3babb23f9d6d56a3baca49f50359e04c
Eddy <eddy@example.com>
2026-04-30 22:42:33 -0500

    fix

--- veterens-and-military-connected-center/scripts/Veterens-military-connected-center.js
+++ veterens-and-military-connected-center/scripts/Veterens-military-connected-center.js
@@ -1,4 +1,4 @@
-document.getElementById("aboutLink").addEventListener("click", function(event) {
+document.getElementById("VMCC").addEventListener("click", function(event) {
     event.preventDefault();
     alert("your are leaving Veterans and Military Connected Center page, this is an external site.");
 });
